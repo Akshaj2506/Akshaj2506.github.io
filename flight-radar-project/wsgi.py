a1e274e0f5181b4a49d787a471549cdbb7364e92
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2024-11-15 21:46:13 +0530

    renamed files

--- flight-radar-project/wsgi.py
+++ flight-radar-project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for flight_radar_project project.
+WSGI config for flight-radar-project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flight_radar_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flight-radar-project.settings')
 
 application = get_wsgi_application()
